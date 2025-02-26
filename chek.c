d152c040483463b3abf61643b03e866ec5bc0f72
erretby39 <erretby37@gmail.com>
2025-02-26 20:04:17 +0000

    Add files via upload

---
+++ chek.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chek.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soer-ret <soer-ret@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 00:36:17 by soer-ret          #+#    #+#             */
+/*   Updated: 2025/02/26 19:43:03 by soer-ret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int chek_sorte(t_list *a)
+{
+    while (a->next)
+    {
+        if(a->content > a->next->content)
+            return(0);
+        a = a->next;
+    }
+    return (1);
+}
+int chek_dup(t_list *a)
+{
+    t_list *chek;
+
+    while (a->next)
+    {
+        chek = a->next;
+        while(chek)
+        {
+            if(a->content == chek->content)
+            {
+                printf("ther is a dup\n");
+                return(0);
+            }
+            chek = chek->next;
+        }
+        a = a->next;
+    }
+    return (1);
+}
